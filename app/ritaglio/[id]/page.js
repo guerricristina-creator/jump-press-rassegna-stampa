a5841a41fee2de4e77fc4b399cccc5ac87a46092
guerricristina-creator <guerri_cristina@libero.it>
2026-08-24 11:55:29 +0200

    Prevent PDF viewer redirect loop

--- app/ritaglio/[id]/page.js
+++ app/ritaglio/[id]/page.js
@@ -4,6 +4,6 @@ export default async function Ritaglio({params}){
   const {id}=await params;
   return <div className="pdfviewer">
     <div className="pdfbar"><div className="pdfbrand"><i>JUMP</i> PRESS <span>· RITAGLIO COMPLETO</span></div><CloseButton /></div>
-    <iframe className="pdfframe" src={`/ritagli/${encodeURIComponent(id)}.pdf`} title="Ritaglio completo" />
+    <iframe className="pdfframe" src={`/ritagli/${encodeURIComponent(id)}.pdf?raw=1`} title="Ritaglio completo" />
   </div>
 }
