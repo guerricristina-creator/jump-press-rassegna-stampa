9f474b4488f8c23e5be5b4bab61682f11628892e
guerricristina-creator <guerri_cristina@libero.it>
2026-08-24 09:56:53 +0200

    Add site layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'Vercel Rassegna Stampa 24 Agosto',description:'Rassegna stampa Juventus del 24 agosto 2026'};
+export default function RootLayout({children}){return <html lang="it"><body>{children}</body></html>}
